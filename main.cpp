8ef70293f061726d02f1b886804d266ded8cf9bc
Raymond Langer <raymolanger@gmail.com>
2024-03-21 12:07:49 -0400

    Added predicted file, and loop to run over detected files with string formatting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include "read_file.h"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 using namespace netCDF;
 
 int main(){
 
-    std::string filename = "gwdata/detection01.nc";
+    std::string filename_predict = "gwdata/GWprediction.nc";
 
-    rvector<std::complex<double>> detect = read_file(filename);
+    rvector<std::complex<double>> predict = read_file(filename_predict);
 
-    std::cout << detect << "\n";
+    for (int i = 1; i < 33; i ++){
+        std::ostringstream string_setup;
+        string_setup << "gwdata/detection" << std::setw(2) << std::setfill('0') << i;
+        std::string filename_detect = string_setup.str();
+        std::cout << filename_detect << "\n"; 
+    }
     return 0;
 }
